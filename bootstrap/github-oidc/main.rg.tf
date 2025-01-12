6c67733051a2c7aee8349e943bb18124772773a4
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-12 14:45:21 +0000

    fix: rg parent

--- bootstrap/github-oidc/main.rg.tf
+++ bootstrap/github-oidc/main.rg.tf
@@ -7,5 +7,5 @@ resource "azapi_resource" "rg" {
   type      = "Microsoft.Resources/resourceGroups@2024-07-01"
   name      = random_id.rg.hex
   location  = "uksouth"
-  parent_id = provider::azapi::build_resource_id("/", "subscriptions", var.subscription_id)
+  parent_id = "/subscriptions/${var.subscription_id}"
 }
